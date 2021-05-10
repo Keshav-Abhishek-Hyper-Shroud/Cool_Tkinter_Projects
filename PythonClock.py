b145e8ced2efc4f0b8aa4f3fb045cc4d65d337d5
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2021-05-11 00:56:12 +0530

    Updated

--- PythonClock.py
+++ PythonClock.py
@@ -3,15 +3,15 @@ from time import time,strftime,gmtime
 from tkinter import *
 
 k=int(time())+19800 # 19800 time gap in seconds acc. to "UTC"
-k=strftime('%A:%B:%Y;%H:%M:%S %p',gmtime(k))
+k=strftime('%A, %d:%B:%Y;%H:%M:%S %p',gmtime(k))
 
 root=Tk()
-root.title('Python Clock')
+root.title('Clock')
 root.resizable(height=False,width=False)
 
 def updateclock():
 	k=int(time())+19800
-	k=strftime('%A:%B:%Y;%H:%M:%S %p',gmtime(k))
+	k=strftime('%A, %d:%B:%Y;%H:%M:%S %p',gmtime(k))
 	label['text']=k.split(';')[1]
 	data='\n'.join(k.split(';')[0].split(':'))
 	daymonthyear_label['text']=data
@@ -26,3 +26,5 @@ daymonthyear_label.pack(pady=10)
 
 updateclock()
 root.mainloop()
+
+#Thanks For Using.
