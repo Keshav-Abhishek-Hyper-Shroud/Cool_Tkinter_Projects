d2d9aeef63e7d6ccde4a9be7adb35776be158664
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2022-01-14 22:29:18 +0530

    Commited

---
+++ Image-Resizer.py
@@ -0,0 +1,135 @@
+import os
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image
+
+root=Tk()
+
+root.geometry('500x500')
+root.title('Image Resizer')
+
+def browsePhoto():
+    global imgLocation
+    imgLocation = filedialog.askopenfilename(title='Open Image File', filetypes=[('PNG Files', '*.png'), ('JPG Files', '*.jpg'), ('JPEG Files', '*.jpeg')])
+
+    if imgLocation:
+        imgName = imgLocation.split('/')[-1]
+        imageNameLabel.config(text=f'Image Name: {imgName}')
+
+        imgWidth,imgHeight = Image.open(imgLocation).size
+
+        imageAspectLabel.config(text=f'Width: {imgWidth}px\nHeight: {imgHeight}px\nAspect-Ratio (Width:Height) = {round(imgWidth/imgHeight,3)}')
+
+        global ratio
+        ratio = round(imgWidth/imgHeight,3)
+
+        widthEntry.config(state=NORMAL)
+        heightEntry.config(state=NORMAL)
+        resizeSave.config(state=NORMAL)
+        browse_photo.config(state=DISABLED)
+    else:
+        imageNameLabel.config(text=f'Image Name: no-file-choosen')
+        imageAspectLabel.config(text=f'')
+        widthEntry.config(state=DISABLED)
+        heightEntry.config(state=DISABLED)
+        resizeSave.config(state=DISABLED)
+        browse_photo.config(state=NORMAL)
+
+browse_photo = Button(root,text='Browse Image',font='times 20 bold',command=browsePhoto)
+browse_photo.pack(pady=20)
+
+main_frame = Frame(root)
+main_frame.pack(pady=20)
+
+imageNameLabel = Label(main_frame,text='Image Name: no-file-choosen')
+imageNameLabel.pack(pady=10)
+
+imageAspectLabel = Label(main_frame,text='')
+imageAspectLabel.pack(pady=10)
+
+userEntryLabel = Label(main_frame,text='User Entry',font='times 20 bold')
+userEntryLabel.pack(pady=5)
+
+frame_2 = Frame(main_frame)
+frame_2.pack(pady=10)
+
+
+widthEntryLabel = Label(frame_2,text='Width: ',font='times 15 bold')
+widthEntryLabel.grid(row=0,column=0)
+
+heightEntryLabel = Label(frame_2,text='Height: ',font='times 15 bold')
+heightEntryLabel.grid(row=1,column=0)
+
+
+
+widthEntry = Entry(frame_2,font='times 15 normal', state=DISABLED)
+widthEntry.grid(row=0,column=1)
+
+heightEntry = Entry(frame_2,font='times 15 normal', state=DISABLED)
+heightEntry.grid(row=1,column=1)
+
+
+
+onlyIntergersAllowedLabelWidth = Label(frame_2,text='(Only Intergers)',font='times 12 normal',foreground='red')
+onlyIntergersAllowedLabelWidth.grid(row=0,column=2)
+
+onlyIntergersAllowedLabelHeight = Label(frame_2,text='(Only Intergers)',font='times 12 normal',foreground='red')
+onlyIntergersAllowedLabelHeight.grid(row=1,column=2)
+
+
+
+def widthControl(e):
+    try:
+        global ratio
+        widthEntry.delete(0,END)
+        width = int(ratio*int(heightEntry.get()+e.char))
+
+        widthEntry.insert(0,str(width))
+    except ValueError:
+        pass
+
+def heightControl(e):
+    try:
+        global ratio
+        heightEntry.delete(0,END)
+        height =int(int(widthEntry.get()+e.char)/ratio)
+
+        heightEntry.insert(0,str(height))
+    except ValueError:
+        pass
+
+widthEntry.bind('<Key>',heightControl)
+heightEntry.bind('<Key>',widthControl)
+
+def saveImage():
+    global imgLocation
+    try:
+        if int(widthEntry.get()) and int(heightEntry.get()):
+            img = Image.open(imgLocation)
+            imgSave = img.resize((int(widthEntry.get()),int(heightEntry.get())))
+            imgSaveLoaction = filedialog.asksaveasfilename(title='Save As', filetypes=[('PNG Files', '*.png'), ('JPG Files', '*.jpg'), ('JPEG Files', '*.jpeg')],defaultextension="")
+            imgSave.save(imgSaveLoaction)
+            os.startfile(imgSaveLoaction)
+            resetApp()
+    except:
+        resetApp()
+
+resizeSave = Button(main_frame,text='Resize & Save',font='times 18 bold',command=saveImage,state=DISABLED)
+resizeSave.pack(pady=10,fill=X)
+
+global resetApp
+def resetApp():
+    imageNameLabel.config(text=f'Image Name: no-file-choosen')
+    imageAspectLabel.config(text=f'')
+    widthEntry.delete(0,END)
+    heightEntry.delete(0,END)
+    widthEntry.config(state=DISABLED)
+    heightEntry.config(state=DISABLED)
+    resizeSave.config(state=DISABLED)
+    browse_photo.config(state=NORMAL)
+
+
+resetAppButton = Button(main_frame,text='Reset',font='times 15 normal',command=resetApp,foreground='red',borderwidth=0)
+resetAppButton.pack(pady=10)
+
+root.mainloop()
--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 ## I learned that computer science is not just about syntax and coding. We can "make a difference" in people's lives by developing applications...
 
-* Link to: [Python Clock.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/PythonClock.py) & [Python Clock Coding Video](https://youtu.be/j-ZG7eh8JLw)
+* Link to: [Python Clock.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/PythonClock.py) & [Python Clock Coding Video](https://youtu.be/j-ZG7eh8JLw)
 
-* Link to: [Calculator.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/Calculator/Python%20Calculator.py) & [Calculator.exe](https://drive.google.com/file/d/180k4q43OOeMZLclWBD9qUb_rf9LHDGR6/view?usp=sharing)
+* Link to: [Calculator.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/Calculator/Python%20Calculator.py) & [Calculator.exe](https://drive.google.com/file/d/180k4q43OOeMZLclWBD9qUb_rf9LHDGR6/view?usp=sharing)
 
-* Link to: [Photo Type Changer.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/Photo%20Type%20Changer.py) & [Photo Type Changer.exe](https://drive.google.com/file/d/1sa0hpIMbWGrNbuxem_TZ-HekbQFvdx4Q/view?usp=sharing)
+* Link to: [Photo Type Changer.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/Photo%20Type%20Changer.py) & [Photo Type Changer.exe](https://drive.google.com/file/d/1sa0hpIMbWGrNbuxem_TZ-HekbQFvdx4Q/view?usp=sharing)
 
-* Link to: [Screenshot Capture.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/ScreenShot%20Capture.py)
+* Link to: [Screenshot Capture.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/ScreenShot%20Capture.py)
 
-* Link to: [Snake Ladder.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/Snake%20Ladder.py) & [Snake Ladder.exe](https://drive.google.com/file/d/11y4BeQq2ISwu5X3DctqE2_Z5uV7TCy15/view?usp=sharing)
+* Link to: [Snake Ladder.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/Snake%20Ladder.py) & [Snake Ladder.exe](https://drive.google.com/file/d/11y4BeQq2ISwu5X3DctqE2_Z5uV7TCy15/view?usp=sharing)
 
-* Link to: [Microsoft_Adobe_Utils.py](https://github.com/Keshav-Abhishek-Hyper-Shroud/Cool_Tkinter_Projects/blob/master/Microsoft_Adobe_Utils.py) & [Microsoft_Adobe_Utils.exe](https://drive.google.com/file/d/1bmUiISNc33EWn8lXFAx6tipN7PPL-tKA/view?usp=sharing)
+* Link to: [Microsoft_Adobe_Utils.py](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/Microsoft_Adobe_Utils.py) & [Microsoft_Adobe_Utils.exe](https://drive.google.com/file/d/1bmUiISNc33EWn8lXFAx6tipN7PPL-tKA/view?usp=sharing)
+
+* Link to: [Image Resizer](https://github.com/KeshavAbhishek/Cool_Tkinter_Projects/blob/master/Image-Resizer.py)
 
 #
 # *Codemy.com*
